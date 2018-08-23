703a5df232b2c8a69444de419d7bbb03cb5173ad
Babak Chehraz <bchehraz@csumb.edu>
2018-08-23 12:55:19 -0700

    Modified IndexPage to show a list of blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,61 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import PropTypes from 'prop-types'
+import '../styles/blog-listing.css'
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+export default function Index({ data }) {
+  const { edges: posts } = data.allMarkdownRemark;
 
-export default IndexPage
+  return (
+    <div className="blog-posts">
+      {posts
+        .filter(post => post.node.frontmatter.title.length > 0)
+        .map(({ node: post }, index) => {
+          if (index === 0) {
+            return (
+              <div className="blog-post-preview-main" key={post.id}>
+                <h1>
+                  <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+                </h1>
+                <h2>{post.frontmatter.date}</h2>
+              </div>
+            );
+          } else {
+            return (
+              <div className="blog-post-preview" key={post.id}>
+                <h1>
+                  <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+                </h1>
+                <h2>
+                  {post.frontmatter.date}
+                </h2>
+              </div>
+            )
+          }
+        })
+      }
+    </div>
+  )
+}
+
+Index.propTypes = {
+   data: PropTypes.object
+}
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "MMMM DD, YYYY")
+            path
+          }
+        }
+      }
+    }
+  }
+`;
