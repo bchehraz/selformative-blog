523013072c29851c869848b7a4668d8ac858db8b
Babak Chehraz <bchehraz@csumb.edu>
2018-08-23 11:46:49 -0700

    Copied the header from previous project

--- src/components/header.js
+++ src/components/header.js
@@ -1,38 +1,81 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import PropTypes from 'prop-types';
+import Img from 'gatsby-image';
 
-const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+//import Menu from '../Menu';
+//import MenuButton from '../MenuButton';
+//import EmailList from '../EmailList';
+
+class Header extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = { visible: false };
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu() {
+    this.setState({
+      visible: !this.state.visible
+    });
+  }
+
+  handleMouseDown(e) {
+    this.toggleMenu();
+    e.stopPropagation();
+  }
+
+  render() {
+    return (
+      <div
+        style={{
+          //background: '#030301',
+          marginBottom: '2rem',
+        }}
+      >
+        <div
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            margin: '0 auto',
+            maxWidth: '100%',
+            padding: '1.45rem 1.0875rem',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            display: 'flex',
+            flexDirection: 'row',
+            background: 'white',
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
-)
 
-Header.propTypes = {
-  siteTitle: PropTypes.string.isRequired,
+          {/*<Nav style={{ flex: 1, textAlign: 'right' }}/> */}
+          <div style={{ flex: 1 }}>
+            <Img
+              className="HeaderLogo"
+              title="Header Image"
+              alt="selformative logo"
+              sizes={this.props.headerImage.sizes}
+              style={{
+                // display: 'flex',
+                // flex: 2,
+                maxWidth: 400,
+                width: 200,
+                margin: 'auto',
+              }}
+            />
+          </div>
+          <div style={{ flex: 1 }} />
+        </div>
+      </div>
+    );
+  }
+}
+
+const styles = {
+  navStyle: {
+    textAlign: 'center',
+    display: 'flex',
+    flex: 1,
+    margin: 'auto',
+  }
 }
 
 export default Header
