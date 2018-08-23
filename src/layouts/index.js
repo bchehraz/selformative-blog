5e382e153630029136fb06f82546fa9cad7d1ee6
Babak Chehraz <bchehraz@csumb.edu>
2018-08-23 12:54:05 -0700

    Updated main page layout to match previous project

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -15,15 +15,22 @@ const Layout = ({ children, data }) => (
       ]}
     />
     <Header headerImage={data.headerImage} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
+    <div>
+      <div
+        style={{
+          margin: "0 auto",
+          maxWidth: 980,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+          height: "100%",
+          padding: "25px"
+        }}
+      >
+        <div style={{ flex: 1 }}>
+          {children()}
+        </div>
+      </div>
     </div>
   </div>
 )
