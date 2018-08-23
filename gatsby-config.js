51dfc5f70d0655a27dda3bae0f48309cdd2e1ba1
Babak Chehraz <bchehraz@csumb.edu>
2018-08-23 11:29:48 -0700

    Added sample blog post with template. Some code copied from previous project

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,39 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Selformative Blog',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-eslint'
+    'gatsby-plugin-eslint',
+    'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|cache|public)/,
+        options: {
+          emitWarning: true,
+          failOnError: false
+        }
+      }
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
+    'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `img`,
+        path: `${__dirname}/src/img`
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
   ],
   pathPrefix: '/selformative-blog',
 }
--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,68 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
- // You can delete this file if you're not using it
+ const path = require('path');
+ const _ = require('lodash');
+
+ exports.createPages = ({ boundActionCreators, graphql }) => {
+   const { createPage } = boundActionCreators;
+
+   const blogPostTemplate = path.resolve("src/templates/blog-post.js");
+   //const categoryTemplate = path.resolve("src/templates/categories.js");
+
+   return graphql(`
+     {
+       allMarkdownRemark(
+         sort: { order: DESC, fields: [frontmatter___date] }
+         limit: 2000
+       ) {
+         edges {
+           node {
+             excerpt(pruneLength: 250)
+             html
+             frontmatter {
+               date
+               path
+             }
+           }
+         }
+       }
+     }
+   `).then(result => {
+     if (result.errors) {
+       return Promise.reject(result.errors);
+     }
+
+     const posts = result.data.allMarkdownRemark.edges;
+
+     // Create post detail pages
+     posts.forEach(({ node }) => {
+       createPage({
+         path: node.frontmatter.path,
+         component: blogPostTemplate,
+       });
+     });
+
+     // Category/Tag Pages:
+     /*let tags = [];
+     // Iterate through each post, putting all found tags into `tags`
+     _.each(posts, edge => {
+       if (_.get(edge, "node.frontmatter.tags")) {
+         tags = tags.concat(edge.node.frontmatter.tags);
+       }
+     });
+     // Eliminate duplicate tags
+     tags = _.uniq(tags);
+
+     // Make tag pages
+     tags.forEach(tag => {
+       createPage({
+         path: `/categories/${_.kebabCase(tag)}/`,
+         component: categoryTemplate,
+         context: {
+           tag,
+         },
+       });
+     });*/
+   });
+ }
