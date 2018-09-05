a0eca4b5159da1cce458ca4d54e2feffa7047b59
Babak Chehraz <bchehraz@csumb.edu>
2018-09-05 11:53:19 -0700

    Added the following to layout: 1. footer with links 2. media query 3. typography with a theme

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import Media from 'react-media'
+import Link from 'gatsby-link';
 
-import Header from '../components/header'
+import Header from '../components/Header'
 import './index.css'
+import '../styles/layout-override.css'
+
+import Typography from 'typography'
+import kirkhamTheme from 'typography-theme-kirkham'
+
+
+const typography = new Typography(kirkhamTheme)
+
+// Output CSS as string.
+typography.toString()
+
+// Or insert styles directly into the <head> (works well for client-only
+// JS web apps.
+typography.injectStyles()
 
 const Layout = ({ children, data }) => (
   <div>
@@ -16,21 +32,61 @@ const Layout = ({ children, data }) => (
     />
     <Header headerImage={data.headerImage} />
     <div>
-      <div
-        style={{
-          margin: "0 auto",
-          maxWidth: 980,
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "space-between",
-          height: "100%",
-          padding: "25px"
-        }}
-      >
-        <div style={{ flex: 1 }}>
-          {children()}
+      <Media query={{ maxWidth: 848 }}>
+        {matches =>
+          matches ? (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+              <div style={{ flex: 1 }}>
+                  {children()}
+              </div>
+            </div>
+          ) : (
+            <div
+              style={{
+                margin: "0 auto",
+                maxWidth: 980,
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-between",
+                height: "100%",
+                padding: "25px"
+              }}
+            >
+            <div style={{ flex: 2.5, paddingRight: "30px" }}>
+              {children()}
+              </div>
+            </div>
+          )
+        }
+      </Media>
+    </div>
+    <div style={{
+      textAlign: 'center',
+      padding: '18px',
+      backgroundColor: 'white',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+    }}>
+      <footer style={{ flex: 1, maxWidth: 980 }}>
+        <div style={{ padding: '18px',  borderTop: '1px solid black' }}>
+          <Link style={{ padding: '5px' }} to="/">{`Contact`}</Link>
+          <Link style={{ padding: '5px' }} to="/">{`Terms of Service`}</Link>
+          <Link style={{ padding: '5px' }} to="/">{`Privacy Policy`}</Link><br />
         </div>
-      </div>
+        Selformative &copy; 2018
+
+      </footer>
     </div>
   </div>
 )
