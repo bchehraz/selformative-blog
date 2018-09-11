4ba1f4170e6f9437d91936b556ac78b58eb436d5
Babak Chehraz <bchehraz@csumb.edu>
2018-09-11 14:05:14 -0700

    Refactored some code on Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,9 +5,8 @@ import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
 import Headroom from 'react-headroom';
 import Navigation from '../Navigation/';
-//import Menu from '../Menu';
-//import MenuButton from '../MenuButton';
-//import EmailList from '../EmailList';
+
+import './index.sass';
 
 class Header extends React.Component {
   constructor(props, context) {
@@ -60,13 +59,6 @@ class Header extends React.Component {
               title="Header Image"
               alt="selformative logo"
               sizes={this.props.headerImage.sizes}
-              style={{
-                // display: 'flex',
-                // flex: 2,
-                maxWidth: 400,
-                width: 200,
-                margin: 'auto',
-              }}
             />
             </Link>
           </div>
@@ -83,17 +75,6 @@ class Header extends React.Component {
 Header.propTypes = {
   headerImage: PropTypes.object,
   sizes: PropTypes.object
-}
-
-const styles = {
-  ul: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 0,
-    margin: 0,
-    flex: 1
-  },
 }
 
 export default Header
