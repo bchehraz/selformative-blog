c0253c7ddbf325376014f127917cefc3e2a414ef
Babak Chehraz <bchehraz@csumb.edu>
2018-09-15 00:50:47 -0700

    Changed /categories/ to /blog/categories/

--- gatsby-node.js
+++ gatsby-node.js
@@ -61,7 +61,7 @@
      // Make tag pages
      tags.forEach(tag => {
        createPage({
-         path: `/categories/${_.kebabCase(tag)}/`,
+         path: `/blog/categories/${_.kebabCase(tag)}/`,
          component: categoryTemplate,
          context: {
            tag,
