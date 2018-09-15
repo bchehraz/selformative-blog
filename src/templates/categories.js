bd923222905874fa2dfcbd35705a2686108eef5b
Babak Chehraz <bchehraz@csumb.edu>
2018-09-14 18:07:21 -0700

    Added in categories page. Basically I copied code from /pages/blog.js since it has to look the same but filter different posts.

--- src/templates/categories.js
+++ src/templates/categories.js
@@ -1,18 +1,68 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Link from 'gatsby-link'
+import Img from 'gatsby-image'
 
-const Categories = ({ pathContext, data }) => {
-  const { tag } = pathContext;
-  const { edges: posts } = data.allMarkdownRemark;
-  return (
-    <div>
-    {/*Add in content for the category page here*/}
-    </div>
-  );
+import '../styles/blog-listing.sass'
+
+/*
+  This template is only for /categories/$tag
+  and doesn't work for /categories/
+*/
+class CategoriesTemplate extends React.Component {
+  getTags = (tags) => {
+    if (tags !== null) {
+      return tags.join(", ");
+    }
+    return "No Category Found";
+  }
+  
+  render() {
+    const { tag } = this.props.pathContext;
+    //const { edges: posts } = data.allMarkdownRemark;
+    const { edges: posts } = this.props.data.allMarkdownRemark;
+
+    return (
+      <div className="blog-listing-container">
+        {posts
+          .filter(post => post.node.frontmatter.title.length > 0)
+          .map(({ node: post }, index) => {
+            const title = post.frontmatter.title;
+            const tags = post.frontmatter.tags;
+
+            return (
+              <div
+                className="blog-listing-outer"
+                onClick={() => push(post.frontmatter.path)}
+                key={post.id}
+              >
+                <div className="blog-listing">
+                  <div className="blog-listing-text" key={post.id}>
+                    <h4 className="category">{this.getTags(tags)}</h4>
+                    <h1>
+                      {title}
+                    </h1>
+                    <h2 className="date" >{post.frontmatter.date}</h2>
+
+                    <p dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+                  </div>
+                  <div className="blog-listing-image-container">
+                    <Img
+                      sizes={post.frontmatter.featuredImage.childImageSharp.sizes}
+                      className="blog-listing-image"
+                    />
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    );
+  }
 }
 
-Categories.propTypes = {
+CategoriesTemplate.propTypes = {
   pathContext: PropTypes.shape({
     tag: PropTypes.string.isRequired,
   }),
@@ -33,23 +83,30 @@ Categories.propTypes = {
   }),
 };
 
-export default Categories;
+export default CategoriesTemplate;
 
 export const pageQuery = graphql`
   query CategoryPage($tag: String) {
     allMarkdownRemark(
-      limit: 2000
       sort: { order: DESC, fields: [frontmatter___date]}
       filter: { frontmatter: { tags: { in : [$tag] } } }
     ) {
       edges {
         node {
-          excerpt(pruneLength: 250)
           id
+          excerpt(pruneLength: 250)
           frontmatter {
             title
             date(formatString: "MMMM DD, YYYY")
             path
+            tags
+            featuredImage {
+              childImageSharp {
+                sizes(maxWidth: 630) {
+                  ...GatsbyImageSharpSizes
+                }
+              }
+            }
           }
         }
       }
