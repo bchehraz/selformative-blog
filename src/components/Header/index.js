9bc474c9f07574d59e49e275ca9d2ab4750bdfc4
Babak Chehraz <bchehraz@csumb.edu>
2018-09-22 12:27:35 -0700

    Made structural changes to Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,8 @@
 import React from 'react';
-
 import Link from 'gatsby-link'
 import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
-import Headroom from 'react-headroom';
+
 import Navigation from '../Navigation/';
 
 import './index.sass';
@@ -28,33 +27,20 @@ class Header extends React.Component {
   }
 
   render() {
-
     return (
-
-      <div
-        style={{
-          //background: '#030301',
-          marginBottom: '0',
-        }}
-      >
-        <Headroom>
-        <div className="header-container">
-          {/*<Nav style={{ flex: 1, textAlign: 'right' }}/> */}
-          <div style={{ flex: 1 }}>
-            <Link to="/">
+      <div className="header-container">
+        <div style={{ flex: 1 }}>
+          <Link to="/">
             <Img
               className="HeaderLogo"
               title="Header Image"
               alt="selformative logo"
               sizes={this.props.headerImage.sizes}
             />
-            </Link>
-          </div>
-          <div style={{ flex: 1 }}>
-            <Navigation />
-          </div>
+          </Link>
         </div>
-        </Headroom>
+        <div className="filler"/>
+        <Navigation />
       </div>
     );
   }
