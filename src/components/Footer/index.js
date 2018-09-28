a10b4a039a9deaf1c625a9c755e89eec859e08fa
Babak Chehraz <bchehraz@csumb.edu>
2018-09-27 18:36:10 -0700

    Remade footer to make it basic and show up normally

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,54 +1,34 @@
 import React from 'react'
 import Media from 'react-media'
 import Link from 'gatsby-link'
+import styled from 'styled-components'
+
+const FooterWrapper = styled.div`
+  text-align: center
+  padding: 0
+  background-color: white
+  display: flex
+  flex-direction: row
+  justify-content: center
+  font-size: 1em
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 200px;
+  zIndex: 3
+`
 
 const Footer = () => (
-  <Media query={{ maxWidth: 480 }}>
-    {matches =>
-      matches ? (
-        <div style={{
-          textAlign: 'center',
-          padding: '18px',
-          backgroundColor: 'white',
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'center',
-          fontSize: '1em'
-        }}>
-          <footer style={{ flex: 1, maxWidth: 980 }}>
-            <div style={{ padding: 18, display: 'flex', justifyContent: 'center', flexFlow: 'column', }}>
-              <Link style={styles.linkStyle} to="/">{`Contact`}</Link>
-              <Link style={styles.linkStyle} to="/">{`Terms of Service`}</Link>
-              <Link style={styles.linkStyle} to="/">{`Privacy Policy`}</Link>
-            </div>
-            Selformative &copy; 2018
-
-          </footer>
-        </div>
-      ) :
-      (
-      <div style={{
-        textAlign: 'center',
-        padding: '18px',
-        backgroundColor: 'white',
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'center',
-        fontSize: '1em'
-      }}>
-        <footer style={{ flex: 1, maxWidth: 980 }}>
-          <div style={{ padding: 18, display: 'flex', justifyContent: 'center', flexFlow: 'row', }}>
-            <Link style={styles.linkStyle} to="/">{`Contact`}</Link>
-            <Link style={styles.linkStyle} to="/">{`Terms of Service`}</Link>
-            <Link style={styles.linkStyle} to="/">{`Privacy Policy`}</Link>
-          </div>
-          {`Selformative`} &copy; {`2018`}
-
-        </footer>
+  <FooterWrapper>
+    <footer style={{ flex: 1, maxWidth: '100vw', zIndex: 3 }}>
+      <div style={{ padding: 18, display: 'flex', justifyContent: 'center', flexFlow: 'column', }}>
+        <Link style={styles.linkStyle} to="/">{`Contact`}</Link>
+        <Link style={styles.linkStyle} to="/">{`Terms of Service`}</Link>
+        <Link style={styles.linkStyle} to="/">{`Privacy Policy`}</Link>
       </div>
-    )
-    }
-  </Media>
+      Selformative &copy; 2018
+    </footer>
+  </FooterWrapper>
 )
 
 const styles = {
