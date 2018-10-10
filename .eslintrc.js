807ffea928217648a3ad51f6276f501d797b1755
Babak Chehraz <bchehraz@csumb.edu>
2018-10-09 20:19:51 -0700

    Added new eslint stuff to maintain better code

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "extends": "airbnb",
+  "settings": {
+   "import/core-modules": ["react", "prop-types", "config"]
+  },
+  "rules": {
+    "react/jsx-filename-extension": 0
+  }
+};
