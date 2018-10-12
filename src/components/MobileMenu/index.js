0b9679fdd1fd7cfafeb5dc98ce15c92e5d7068ab
Babak Chehraz <bchehraz@csumb.edu>
2018-10-11 17:12:04 -0700

    Modified file to match new eslint rules

--- src/components/MobileMenu/index.js
+++ src/components/MobileMenu/index.js
@@ -1,58 +1,69 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import { bubble as Menu } from 'react-burger-menu'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Link from 'gatsby-link';
+import { bubble as Menu } from 'react-burger-menu';
 
 import './index.sass';
 
-//TODO: Refactor unnecessary closeMenu code since it seems to work without it
+// TODO: Refactor unnecessary closeMenu code since it seems to work without it
 
 class MobileMenu extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       isOpen: false,
-      pinned: false
-    }
+      pinned: false,
+    };
 
-    this.closeMenu = this.closeMenu.bind(this)
-    this.setPin = this.setPin.bind(this)
+    this.closeMenu = this.closeMenu.bind(this);
+    this.setPin = this.setPin.bind(this);
   }
 
   componentWillReceiveProps(props) {
-    this.setPin(props.pinned)
+    this.setPin(props.pinned);
+  }
+
+  setPin(status) {
+    const { pinned } = this.state;
+    if (status !== pinned) {
+      this.setState({
+        pinned: status,
+      });
+    }
   }
 
   closeMenu() {
     this.setState({
-      isOpen: false
-    })
-  }
-
-  setPin(status) {
-    if (status !== this.state.pinned) {
-      this.setState({
-        pinned: status
-      })
-    }
+      isOpen: false,
+    });
   }
 
   render() {
+    const { isOpen, pinned } = this.state;
     return (
-      <div className={(this.state.pinned) ? "pinned" : "unpinned"}>
+      <div className={(pinned) ? 'pinned' : 'unpinned'}>
         <Menu
-          isOpen={this.state.isOpen}
-          pageWrapId={"page-wrap"}
-          outerContainerId={"outer-container"}
+          isOpen={isOpen}
+          pageWrapId="page-wrap"
+          outerContainerId="outer-container"
           left
         >
-          <Link onClick={this.closeMenu} id="home" className="menu-item" to="/blog">{`Blog`}</Link>
-          <Link id="about" className="menu-item"  to="/about">{`About Me`}</Link>
-          <Link id="contact" className="menu-item" to="/lets-chat">{`Let's Chat`}</Link>
+          <Link onClick={this.closeMenu} id="home" className="menu-item" to="/blog">Blog</Link>
+          <Link id="about" className="menu-item" to="/about">About Me</Link>
+          <Link id="contact" className="menu-item" to="/lets-chat">Let&#39;s Chat</Link>
         </Menu>
       </div>
-    )
+    );
   }
 }
 
-export default MobileMenu
+MobileMenu.defaultProps = {
+  pinned: false,
+};
+
+MobileMenu.propTypes = {
+  pinned: PropTypes.bool,
+};
+
+export default MobileMenu;
