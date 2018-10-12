d821bb44ac63fec8323d7b2b18f31bc61cf0ddc3
Babak Chehraz <bchehraz@csumb.edu>
2018-10-11 21:27:00 -0700

    Refactored code to match eslint reqs

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,55 +1,52 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-import Media from 'react-media'
-import Link from 'gatsby-link';
+import React from 'react';
+import Helmet from 'react-helmet';
 import Headroom from 'react-headroom';
 
-import Header from '../components/Header'
-import Footer from '../components/Footer'
-import MobileMenu from '../components/MobileMenu'
-import './index.sass'
+import Typography from 'typography';
+import deyoungTheme from 'typography-theme-de-young';
+// import parnassusTheme from 'typography-theme-parnassus'
+
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import MobileMenu from '../components/MobileMenu';
+import './index.sass';
 // import '../styles/layout-override.css'
 
-import Typography from 'typography'
-import deyoungTheme from 'typography-theme-de-young'
-//import parnassusTheme from 'typography-theme-parnassus'
-
-const typography = new Typography(deyoungTheme)
+const typography = new Typography(deyoungTheme);
 
 // Output CSS as string.
-typography.toString()
+typography.toString();
 
 // Or insert styles directly into the <head> (works well for client-only
 // JS web apps.
-typography.injectStyles()
+typography.injectStyles();
 
 class Layout extends React.Component {
-
   constructor(props) {
     super(props);
 
     this.state = {
       header: {
-        pinned: true
-      }
-    }
-    this.onPin = this.onPin.bind(this)
-    this.onUnpin = this.onUnpin.bind(this)
+        pinned: true,
+      },
+    };
+    this.onPin = this.onPin.bind(this);
+    this.onUnpin = this.onUnpin.bind(this);
   }
 
   onPin() {
-    this.setState({ header: { pinned: true }})
+    this.setState({ header: { pinned: true } });
   }
 
-  onUnpin() { //header pin
-    this.setState({ header: { pinned: false }})
+  onUnpin() {
+    this.setState({ header: { pinned: false } });
   }
 
-  render () {
+  render() {
     const { children, data } = this.props;
+    const { header } = this.state;
     return (
-      <div  style={{ background: '#efefef' }} id="outer-container">
+      <div style={{ background: '#efefef' }} id="outer-container">
         <Helmet
           title={data.site.siteMetadata.title}
           meta={[
@@ -57,8 +54,8 @@ class Layout extends React.Component {
             { name: 'keywords', content: 'sample, something' },
           ]}
         />
-        <MobileMenu pinned={this.state.header.pinned} />
-        <div id="page-wrap" style={{ position: 'relative', }}>
+        <MobileMenu pinned={header.pinned} />
+        <div id="page-wrap" style={{ position: 'relative' }}>
           <Headroom
             pinStart={50}
             onPin={this.onPin}
@@ -67,61 +64,20 @@ class Layout extends React.Component {
             <Header headerImage={data.headerImage} />
           </Headroom>
           <div>
-            <Media query={{ maxWidth: 848 }}>
-              {matches =>
-                matches ? (
-                  <div
-                    style={{
-                      margin: "0 auto",
-                      maxWidth: "100vw",
-                      display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "space-between",
-                      height: "100%",
-                      marginTop: "0",
-                      zIndex: 1,
-                      paddingBottom: '200px',
-                    }}
-                  >
-                    <div style={{ flex: 1 }}>
-                        {children()}
-                    </div>
-                  </div>
-                ) : (
-                  <div
-                    style={{
-                      margin: "0 auto",
-                      maxWidth: "100vw",
-                      display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "space-between",
-                      height: "100%",
-                      marginTop: "0",
-                      paddingBottom: '200px',
-                      zIndex: 1,
-                    }}
-                  >
-                  <div style={{ flex: 1 }}>
-                    {children()}
-                    </div>
-                  </div>
-                )
-              }
-            </Media>
+            <div className="layout-container">
+              <div style={{ flex: 1 }}>
+                {children()}
+              </div>
+            </div>
           </div>
           <Footer />
-        </div>{/* id: page-wrap*/}
+        </div>
       </div>
-    )
+    );
   }
 }
 
-Layout.propTypes = {
-  children: PropTypes.func,
-  data: PropTypes.object
-}
-
-export default Layout
+export default Layout;
 
 export const query = graphql`
   query SiteDataQuery {
@@ -138,4 +94,4 @@ export const query = graphql`
       }
     }
   }
-`
+`;
