1171ccf776b30b9b5ee4011040289dd633e14bf6
Babak Chehraz <bchehraz@csumb.edu>
2018-10-11 19:14:02 -0700

    Refactored for eslint rules

--- src/components/EmailForm/index.js
+++ src/components/EmailForm/index.js
@@ -1,35 +1,28 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link, { push } from 'gatsby-link'
-import Img from 'gatsby-image'
+import React from 'react';
+import Img from 'gatsby-image';
 
-import './index.sass'
+import './index.sass';
 
-class EmailForm extends React.Component {
-  constructor(props, context) {
-    super(props, context)
-  }
-  render() {
-    return (
-      <div className="email-form-container">
-        <Img
-          sizes={this.props.img.sizes}
-          style={{
-            position: 'absolute',
-            left: '74vw',
-            top: '500px',
-            height: '350px',
-            width: '25vw',
-          }}
-        />
-        <form className="email-form">
+const inputId = '123';
 
-          <label>{`Email Address`}</label>
-          <input type="email"/>
-        </form>
-      </div>
-    )
-  }
-}
+const EmailForm = ({ img }) => (
+  <div className="email-form-container">
+    <Img
+      sizes={img.sizes}
+      style={{
+        position: 'absolute',
+        left: '74vw',
+        top: '500px',
+        height: '350px',
+        width: '25vw',
+      }}
+    />
+    <form className="email-form">
 
-export default EmailForm
+      Email Address:
+      <input id={inputId} type="email" />
+    </form>
+  </div>
+);
+
+export default EmailForm;
