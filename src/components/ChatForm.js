caaed8f6bf4afea991aee04ac9a1ec1b2d44c06a
Babak Chehraz <bchehraz@csumb.edu>
2018-10-11 17:50:08 -0700

    Refactored unused code. Modified format to match new eslint rules

--- src/components/ChatForm.js
+++ src/components/ChatForm.js
@@ -1,6 +1,6 @@
 
-import React from 'react'
-import styled, { keyframes } from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Form = styled.form`
   padding: 2em
@@ -13,13 +13,13 @@ const Form = styled.form`
   flex-direction: column
   justify-content: center
   align-items: center
-`
+`;
 
 const FormHeader = styled.h3`
   padding: 0
   margin: 0
   color: black
-`
+`;
 
 const TextInput = styled.input`
   margin: 1em auto
@@ -28,8 +28,7 @@ const TextInput = styled.input`
   color: white
   background-color: #333
   border: none
-  margin-bottom: ${props => props.beganTyping ? '4rem' : '0'}
-`
+`;
 
 const MessageField = styled.textarea`
   width: 100%
@@ -39,13 +38,13 @@ const MessageField = styled.textarea`
   background-color: #333
   border: none
   min-height: 200px
-`
+`;
 
 const CharLimit = styled.div`
   border-radius: 5px
   display: inline
   padding: 0.5em
-`
+`;
 
 const SendButton = styled.input`
   width: 100%
@@ -53,7 +52,7 @@ const SendButton = styled.input`
   background-color: #4e85ad
   padding: 1em
   margin-top: 1em
-`
+`;
 
 const CharCountContainer = styled.div`
   paddingBottom: 1
@@ -62,14 +61,14 @@ const CharCountContainer = styled.div`
   -ms-transition: all 500ms ease-in-out;
   -o-transition: all 500ms ease-in-out;
   transition: all 500ms ease-in-out;
-  opacity: ${props => props.beganTyping ? 1 : 0}
+  opacity: ${props => props.beganTyping || 0}
   margin: 0 auto
   grid-column: 1;
   grid-row: 1;
-`
+`;
 
 const MessageFieldLabel = styled.h3`
-  opacity: ${props => props.beganTyping ? 0 : 1}
+  opacity: ${props => !props.beganTyping || 0}
   grid-column: 1;
   grid-row: 1;
   -webkit-transition: all 500ms ease-in-out;
@@ -77,48 +76,53 @@ const MessageFieldLabel = styled.h3`
   -ms-transition: all 500ms ease-in-out;
   -o-transition: all 500ms ease-in-out;
   transition: all 500ms ease-in-out;
-`
+`;
 
 class ChatForm extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    const MAXIMUM_MESSAGE_LENGTH = 512
+    const MAXIMUM_MESSAGE_LENGTH = 512;
 
     this.state = {
-      email: "",
-      message: "",
+      // email: '',
+      message: '',
       maxLength: MAXIMUM_MESSAGE_LENGTH,
-      remaining: MAXIMUM_MESSAGE_LENGTH
-    }
+      remaining: MAXIMUM_MESSAGE_LENGTH,
+    };
 
-    this.handleEmailChange = this.handleEmailChange.bind(this)
-    this.handleTextAreaChange = this.handleTextAreaChange.bind(this)
-    this.selectColorBasedOnRemaining = this.selectColorBasedOnRemaining.bind(this)
+    // his.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+    this.selectColorBasedOnRemaining = this.selectColorBasedOnRemaining.bind(this);
   }
 
+  /*
   handleEmailChange(event) {
-    this.setState({ email: event.target.value })
+    this.setState({ email: event.target.value });
   }
+  */
 
   handleTextAreaChange(event) {
+    const { maxLength } = this.state;
+    const { state } = this;
     this.setState({
+      ...state,
       message: event.target.value,
-      remaining: this.state.maxLength - event.target.value.length
-    })
+      remaining: maxLength - event.target.value.length,
+    });
   }
 
   selectColorBasedOnRemaining() {
     const { remaining } = this.state;
-    return `rgb(255, ${remaining > 150 ? 255 : remaining+100}, ${remaining > 200 ? 255 : remaining})`
+    return `rgb(255, ${remaining > 150 ? 255 : remaining + 100}, ${remaining > 200 ? 255 : remaining})`;
   }
 
   render() {
-    const { maxLength, remaining } = this.state
+    const { maxLength, remaining, message } = this.state;
 
     return (
       <Form>
-        <FormHeader>{`Let's Chat`}</FormHeader>
+        <FormHeader>Let&#39;s Chat</FormHeader>
         <TextInput
           type="email"
           placeholder="youremail@email.com"
@@ -128,20 +132,21 @@ class ChatForm extends React.Component {
 
         <div style={{ display: 'grid' }}>
           <MessageFieldLabel beganTyping={remaining !== 512}>
-            {'Your Message'}
+            Your Message
           </MessageFieldLabel>
           <CharCountContainer
             beganTyping={remaining !== 512}
           >
-            <h2>{`Characters Remaining: `}
+            <h2>
+              Characters Remaining:
               <CharLimit
-                status={maxLength - this.state.message.length}
+                status={maxLength - message.length}
                 style={{
                   backgroundColor: this.selectColorBasedOnRemaining(),
-                  color: `${(remaining > 68) ? 'black' : 'white'}`
+                  color: `${(remaining > 68) ? 'black' : 'white'}`,
                 }}
               >
-                {`${maxLength - this.state.message.length}`}
+                {`${maxLength - message.length}`}
               </CharLimit>
             </h2>
           </CharCountContainer>
@@ -155,8 +160,8 @@ class ChatForm extends React.Component {
 
         <SendButton type="button" value="Send" />
       </Form>
-    )
+    );
   }
 }
 
-export default ChatForm
+export default ChatForm;
