e7e32a15dae46bf2dca387218f4e7dfc2114d894
Babak Chehraz <bchehraz@csumb.edu>
2018-10-11 18:30:11 -0700

    Added graphql as global to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,9 @@ module.exports = {
   "settings": {
    "import/core-modules": ["react", "prop-types", "config"]
   },
+  "globals": {
+        "graphql": true
+    },
   "rules": {
     "react/jsx-filename-extension": 0,
     "react/prop-types": 0,
