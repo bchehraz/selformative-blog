7fb6e45ac77ac98c8493d04b08c74c58721d6442
Babak Chehraz <bchehraz@csumb.edu>
2018-10-11 18:07:00 -0700

    Refactored unneeded code. Formatted file to meet eslint reqs

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -2,40 +2,6 @@ import React from 'react';
 import Link from 'gatsby-link';
 import './index.sass';
 
-class Navigation extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-  }
-
-  render() {
-    return (
-      <nav className="nav-container">
-        <div
-          className="navItem"
-          onClick={this.handleClick}
-        >
-          <h2><Link style={styles.linkStyle} to="/about" activeStyle={styles.activeLink}>{`ABOUT ME`}</Link></h2>
-        </div>
-        <div
-          className="navItem"
-          style={styles.navItem}
-          onClick={this.handleClick}
-        >
-          <h2><Link style={styles.linkStyle} to="/blog" activeStyle={styles.activeLink}>{`BLOG`}</Link></h2>
-        </div>
-        <div
-          className="navItem"
-          style={styles.navItem}
-          onClick={this.handleClick}
-        >
-          <h2><Link style={styles.linkStyle} to="/lets-chat" activeStyle={styles.activeLink}>{`LET'S CHAT`}</Link></h2>
-        </div>
-      </nav>
-    );
-  }
-
-}
-
 const styles = {
   linkStyle: {
     color: '#aaa',
@@ -44,7 +10,21 @@ const styles = {
   activeLink: {
     color: '#4286f4',
     borderBottom: '1px solid black',
-  }
-}
+  },
+};
+
+const Navigation = () => (
+  <nav className="nav-container">
+    <div className="navItem">
+      <h2><Link style={styles.linkStyle} to="/about" activeStyle={styles.activeLink}>ABOUT ME</Link></h2>
+    </div>
+    <div className="navItem">
+      <h2><Link style={styles.linkStyle} to="/blog" activeStyle={styles.activeLink}>BLOG</Link></h2>
+    </div>
+    <div className="navItem">
+      <h2><Link style={styles.linkStyle} to="/lets-chat" activeStyle={styles.activeLink}>LET&#39;S CHAT</Link></h2>
+    </div>
+  </nav>
+);
 
 export default Navigation;
