ab9e8fd070008daffc401800e64331d64823104d
Babak Chehraz <bchehraz@csumb.edu>
2018-10-12 18:24:53 -0700

    Refactored for new eslint rules

--- src/templates/categories.js
+++ src/templates/categories.js
@@ -1,87 +1,73 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Link, { push } from 'gatsby-link'
-import Img from 'gatsby-image'
+import React from 'react';
+import Link, { push } from 'gatsby-link';
+import Img from 'gatsby-image';
 
-import '../styles/blog-listing.sass'
+import '../styles/blog-listing.sass';
 
 /*
   This template is only for /category/$tag
   and doesn't work for /category/
 */
-class CategoriesTemplate extends React.Component {
-  getTags = (tags) => {
-    if (tags !== null) {
-      return tags.join(", ");
-    }
-    return "No Category Found";
+
+const getTags = (tags) => {
+  if (tags !== null) {
+    return tags.join(', ');
   }
+  return 'No Category Found';
+};
 
-  render() {
-    const { tag } = this.props.pathContext;
-    //const { edges: posts } = data.allMarkdownRemark;
-    const { edges: posts } = this.props.data.allMarkdownRemark;
+const CategoriesTemplate = ({ data, pathContext }) => {
+  const { tag } = pathContext;
+  const { edges: posts } = data.allMarkdownRemark;
 
-    return (
-      <div className="blog-listing-container">
-        <h2 style={{ flexBasis: '100%', textAlign: 'center' }}>{`Showing posts with category: `} <span style={{ fontWeight: 'bold', }}>{`${tag}`}</span> <Link to="/blog">{`Reset`}</Link></h2>
-        {posts
-          .filter(post => post.node.frontmatter.title.length > 0)
-          .map(({ node: post }, index) => {
-            const title = post.frontmatter.title;
-            const tags = post.frontmatter.tags;
+  return (
+    <div className="blog-listing-container">
+      <h2 style={{ flexBasis: '100%', textAlign: 'center' }}>
+        Showing posts with category:
+        <span style={{ fontWeight: 'bold' }}>{`${tag}`}</span>
+        <Link to="/blog">Reset</Link>
+      </h2>
+      {posts
+        .filter(post => post.node.frontmatter.title.length > 0)
+        .map(({ node: post }) => {
+          const {
+            title,
+            tags,
+            path,
+          } = post.frontmatter;
 
-            return (
-              <div
-                className="blog-listing-outer"
-                onClick={() => push(post.frontmatter.path)}
-                key={post.id}
-              >
-                <div className="blog-listing">
-                  <div className="blog-listing-text" key={post.id}>
-                    <h4 className="category">{this.getTags(tags)}</h4>
-                    <h1>
-                      {title}
-                    </h1>
-                    <h2 className="date" >{post.frontmatter.date}</h2>
+          return (
+            <div
+              className="blog-listing-outer"
+              onClick={() => push(path)}
+              role="button"
+              onKeyPress={() => push(path)}
+              key={post.id}
+              tabIndex={0}
+            >
+              <div className="blog-listing">
+                <div className="blog-listing-text" key={post.id}>
+                  <h4 className="category">{getTags(tags)}</h4>
+                  <h1>
+                    {title}
+                  </h1>
+                  <h2 className="date">{post.frontmatter.date}</h2>
 
-                    <p dangerouslySetInnerHTML={{ __html: post.excerpt }} />
-                  </div>
-                  <div className="blog-listing-image-container">
-                    <Img
-                      sizes={post.frontmatter.featuredImage.childImageSharp.sizes}
-                      className="blog-listing-image"
-                    />
-                  </div>
+                  <p dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+                </div>
+                <div className="blog-listing-image-container">
+                  <Img
+                    sizes={post.frontmatter.featuredImage.childImageSharp.sizes}
+                    className="blog-listing-image"
+                  />
                 </div>
               </div>
-            )
-          })
-        }
-      </div>
-    );
-  }
-}
-
-CategoriesTemplate.propTypes = {
-  pathContext: PropTypes.shape({
-    tag: PropTypes.string.isRequired,
-  }),
-  data: PropTypes.shape({
-    allMarkdownRemark: PropTypes.shape({
-      totalCount: PropTypes.number.isRequired,
-      edges: PropTypes.arrayOf(
-        PropTypes.shape({
-          node: PropTypes.shape({
-            frontmatter: PropTypes.shape({
-              path: PropTypes.string.isRequired,
-              title: PropTypes.string.isRequired,
-            }),
-          }),
-        }).isRequired
-      ),
-    }),
-  }),
+            </div>
+          );
+        })
+      }
+    </div>
+  );
 };
 
 export default CategoriesTemplate;
