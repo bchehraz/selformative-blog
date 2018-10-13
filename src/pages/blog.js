02b613898c62a221a1f821e6566f99531c541de1
Babak Chehraz <bchehraz@csumb.edu>
2018-10-12 18:11:17 -0700

    Refactored for eslint rules

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,66 +1,63 @@
-import React from 'react'
-import { push } from 'gatsby-link'
-import Helmet from 'react-helmet'
-import PropTypes from 'prop-types'
-import Img from 'gatsby-image'
-import Media from 'react-media'
+import React from 'react';
+import { push } from 'gatsby-link';
+import Img from 'gatsby-image';
 
-import '../styles/blog-listing.sass'
+import '../styles/blog-listing.sass';
 
-class Blog extends React.Component {
-  getTags = (tags) => {
-    if (tags !== null) {
-      return tags.join(", ");
-    }
-    return "No Category Found";
+const getTags = (tags) => {
+  if (tags !== null) {
+    return tags.join(', ');
   }
+  return 'No Category Found';
+};
 
-  render() {
-    const { edges: posts } = this.props.data.allMarkdownRemark;
+const Blog = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark;
 
-    return (
-      <div className="blog-listing-container">
-        {posts
-          .filter(post => post.node.frontmatter.title.length > 0)
-          .map(({ node: post }, index) => {
-            const title = post.frontmatter.title;
-            const tags = post.frontmatter.tags;
+  return (
+    <div className="blog-listing-container">
+      {posts
+        .filter(post => post.node.frontmatter.title.length > 0)
+        .map(({ node: post }) => {
+          const {
+            title,
+            tags,
+            path,
+          } = post.frontmatter;
 
-            return (
-              <div
-                className="blog-listing-outer"
-                onClick={() => push(post.frontmatter.path)}
-                key={post.id}
-              >
-                <div className="blog-listing">
-                  <div className="blog-listing-text" key={post.id}>
-                    <h4 className="category">{this.getTags(tags)}</h4>
-                    <h1>
-                      {title}
-                    </h1>
-                    <h2 className="date" >{post.frontmatter.date}</h2>
+          return (
+            <div
+              className="blog-listing-outer"
+              onClick={() => push(path)}
+              role="button"
+              onKeyPress={() => push(path)}
+              key={post.id}
+              tabIndex={0}
+            >
+              <div className="blog-listing">
+                <div className="blog-listing-text" key={post.id}>
+                  <h4 className="category">{getTags(tags)}</h4>
+                  <h1>
+                    {title}
+                  </h1>
+                  <h2 className="date">{post.frontmatter.date}</h2>
 
-                    <p dangerouslySetInnerHTML={{ __html: post.excerpt }} />
-                  </div>
-                  <div className="blog-listing-image-container">
-                    <Img
-                      sizes={post.frontmatter.featuredImage.childImageSharp.sizes}
-                      className="blog-listing-image"
-                    />
-                  </div>
+                  <p dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+                </div>
+                <div className="blog-listing-image-container">
+                  <Img
+                    sizes={post.frontmatter.featuredImage.childImageSharp.sizes}
+                    className="blog-listing-image"
+                  />
                 </div>
               </div>
-            )
-          })
-        }
-      </div>
-    );
-  }
-}
-
-Blog.propTypes = {
-   data: PropTypes.object
-}
+            </div>
+          );
+        })
+      }
+    </div>
+  );
+};
 
 export default Blog;
 
