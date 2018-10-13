fa73af2c25a360b194761aacab9ee2770e449031
Babak Chehraz <bchehraz@csumb.edu>
2018-10-12 17:51:43 -0700

    Modified index.js page to meet eslint rules

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,75 +1,8 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import Helmet from 'react-helmet'
-import PropTypes from 'prop-types'
-import Img from 'gatsby-image'
+import React from 'react';
+import Link from 'gatsby-link';
+import Img from 'gatsby-image';
 
-import '../styles/blog-listing.sass'
-import BgImage from '../components/BgImage';
-
-class Index extends React.Component {
-
-  constructor() {
-    super()
-    let postsToShow = 12
-    if (typeof window !== 'undefined') {
-      postsToShow = window.postsToShow
-    }
-    this.state = {
-      showingMore: postsToShow > 12,
-      postsToShow,
-    }
-  }
-
-  render() {
-    const { edges: posts } = this.props.data.allMarkdownRemark;
-
-    return (
-      <div style={styles.container}>
-        {posts
-          .filter(post => post.node.frontmatter.title.length > 0)
-          .map(({ node: post }, index) => {
-            const title = post.frontmatter.title;
-
-            if (index === 0) {
-              return (
-                <div
-                  className="blog-post-preview-main"
-                  style={styles.blogListingContainer}
-                  key={post.id}
-                >
-                  <h1>
-                    <Link to={post.frontmatter.path}>{title}</Link>
-                  </h1>
-                  <h2>{post.frontmatter.date}</h2>
-                  <Img sizes={post.frontmatter.featuredImage.childImageSharp.sizes} />
-                  <p dangerouslySetInnerHTML={{ __html: post.excerpt }} />
-                </div>
-              );
-            } else {
-              return (
-                <div className="blog-post-preview" style={styles.blogListingContainer} key={post.id}>
-                  <h1>
-                    <Link to={post.frontmatter.path}>{title}</Link>
-                  </h1>
-                  <h2>
-                    {post.frontmatter.date}
-                  </h2>
-                  <Img sizes={post.frontmatter.featuredImage.childImageSharp.sizes} />
-                  <p dangerouslySetInnerHTML={{ __html: post.excerpt }} />
-                </div>
-              )
-            }
-          })
-        }
-      </div>
-    );
-  }
-}
-
-Index.propTypes = {
-   data: PropTypes.object
-}
+import '../styles/blog-listing.sass';
 
 const styles = {
   container: {
@@ -83,8 +16,52 @@ const styles = {
   blogListingContainer: {
     maxWidth: '980px',
     flex: 1,
-  }
-}
+  },
+};
+
+const Index = ({ data }) => {
+  const { edges: posts } = data.allMarkdownRemark;
+
+  return (
+    <div style={styles.container}>
+      {posts
+        .filter(post => post.node.frontmatter.title.length > 0)
+        .map(({ node: post }, index) => {
+          const { title } = post.frontmatter;
+
+          if (index === 0) {
+            return (
+              <div
+                className="blog-post-preview-main"
+                style={styles.blogListingContainer}
+                key={post.id}
+              >
+                <h1>
+                  <Link to={post.frontmatter.path}>{title}</Link>
+                </h1>
+                <h2>{post.frontmatter.date}</h2>
+                <Img sizes={post.frontmatter.featuredImage.childImageSharp.sizes} />
+                <p dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+              </div>
+            );
+          }
+          return (
+            <div className="blog-post-preview" style={styles.blogListingContainer} key={post.id}>
+              <h1>
+                <Link to={post.frontmatter.path}>{title}</Link>
+              </h1>
+              <h2>
+                {post.frontmatter.date}
+              </h2>
+              <Img sizes={post.frontmatter.featuredImage.childImageSharp.sizes} />
+              <p dangerouslySetInnerHTML={{ __html: post.excerpt }} />
+            </div>
+          );
+        })
+      }
+    </div>
+  );
+};
 
 export default Index;
 
